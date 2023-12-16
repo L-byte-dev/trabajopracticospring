fef6dbcbda88567dcf80735e6f0bd3326cdf95e5
Lucas Gabriel Gómez <lucasgabrielg2001@gmail.com>
2023-12-16 14:09:57 -0300

    Primer Commit

---
+++ trabajopracticospring/src/test/java/com/informatorio/trabajopracticospring/TrabajopracticospringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.informatorio.trabajopracticospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrabajopracticospringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
